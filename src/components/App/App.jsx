5c547e0da6a21c69df5c02a7155a935b9ff62e16
Beth Tracy <bntracy@gmail.com>
2024-02-19 16:34:32 -0600

    all routes added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,18 +3,33 @@ import './App.css';
 import StudentForm from '../StudentForm/StudentForm.jsx';
 import StudentList from '../StudentList/StudentList.jsx';
 
+import { HashRouter as Router, Route, Link } from 'react-router-dom/cjs/react-router-dom.min.js';
+
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-            <h1 className="App-title">GitHub Student List</h1>
-            </header>
-            <br/>
-            <StudentForm />
-
-            <p>Student list:</p>
-            <StudentList />
-        </div>
+        <Router>
+            <div className="App">
+                <header className="App-header">
+                <h1 className="App-title">GitHub Student List</h1>
+                </header>
+                <br/>
+                <nav>
+                    <Link to="/home">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/allStudents">Students</Link>
+                </nav>
+                <Route path="/home">
+                    <StudentForm />
+                </Route>
+                <Route path="/about">
+                    <p>A list of student GitHub names.</p>
+                </Route>
+                <Route path="/allStudents">
+                    <p>Student list:</p>
+                    <StudentList />
+                </Route>
+            </div>
+        </Router>
     );
 }
 
